542d11a5268488064cfcd485409ed22c31c4c558
Mubeen Anwar <mubeenanwar260@gmail.com>
2025-04-16 20:42:46 +0500

    Add files via upload

---
+++ Datatypes and Variables/Datatype_and_Variable.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+// There are six datatyes in c++
+// int, char, float, double, bool,string
+// we can make them short or long by using short and long sufix
+// short int (for short integer)
+// long int (for long integer)
+// The syntax is that we write the datatype first and then name of variable(you can give it any name) and then we assign the value to this variable
+
+int num=10;        //it store integer
+double dec=10.1;   //it store long decimal
+float dec2=13.4;   //it store short decimal 
+string str="Roboverse";   //it store sentence
+bool check=true;        //it store true/false(check condition)
+char c='A';          //it store character    
+cout<<num<<endl;
+cout<<dec<<endl;
+cout<<dec2<<endl;
+cout<<str<<endl;
+cout<<check<<endl;
+cout<<c<<endl;
+
+
+
+return 0;
+}
