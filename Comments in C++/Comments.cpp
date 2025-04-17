269b788c8deb057c7469f7f0590ff2ff9771a3e9
Mubeen Anwar <mubeenanwar260@gmail.com>
2025-04-17 08:38:56 +0500

    Add files via upload

---
+++ Comments in C++/Comments.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+// if we are require that the some of the content of our code should not execute then we use comments in c++
+// Comments are of two types 
+// 1. Single line comments(to comment out single line)
+// 2. Multi line comments(to comment out multi line or paragraph)
+
+int main()
+{
+// Single line comment:
+// Hello from Roboverse
+// Multi line comment:
+/*Beginner-friendly C++ programs covering core programming concepts like variables, control structures, functions, arrays, pointers, file handling, and basic OOP. Perfect for students or anyone starting with C++ and looking to build strong programming fundamentals. 🔹 What You’ll Learn: This repository covers a broad range of foundational topics in C++:
+
+Basic Syntax – Learn how to write simple C++ programs, use comments, and understand the structure of a C++ project.
+
+Data Types & Variables – Explore different data types, how variables are declared and initialized, and how type conversion works.
+
+Input/Output – Master standard I/O operations using cin, cout, and formatting output.
+
+Control Structures – Use conditional statements (if, switch) and loops (for, while, do-while) for decision making and repetition.
+
+Functions – Understand function declaration, definition, parameter passing, return types, and scope rules.
+
+Arrays & Strings – Work with single and multi-dimensional arrays and basic string operations.
+
+Pointers – Learn the basics of pointers, memory addresses, and pointer arithmetic.
+
+File Handling – Perform file operations like reading from and writing to text files.
+
+Object-Oriented Programming (OOP) – Get introduced to classes, objects, constructors, and access specifiers*/
+
+
+
+
+return 0;
+}
